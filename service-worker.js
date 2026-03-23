491898b5b9b085e2d096b1f9049975e9a5ab37d4
iSmile <ismailhromcik@proton.me>
2026-03-23 05:24:36 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,6 +5,10 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   console.log("Service Worker activated");
+
+  event.waitUntil(
+    self.clients.claim()
+  );
 });
 
 self.addEventListener("fetch", event => {
