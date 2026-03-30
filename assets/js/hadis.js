1b63c4d09b2ac4528204997aa9870a4c6503c771
iSmile <ismailhromcik@proton.me>
2026-03-30 15:33:28 +0000

    Update hadis.js

--- assets/js/hadis.js
+++ assets/js/hadis.js
@@ -53,16 +53,22 @@ document.addEventListener("DOMContentLoaded", async () => {
     showAll = (broj === "");
   }
 
-  if (
-    zbirka === "muslim" &&
-    broj &&
-    !isNaN(broj) &&
-    Number(broj) >= 1 &&
-    Number(broj) <= 7
-  ) {
-    window.location.href = "zbirke/muslim/uvod.html";
-    return;
-  }
+  
+if (
+  zbirka === "muslim" &&
+  (
+    broj === "uvod" ||
+    (
+      broj &&
+      !isNaN(broj) &&
+      Number(broj) >= 1 &&
+      Number(broj) <= 7
+    )
+  )
+) {
+  window.location.href = "zbirke/muslim/uvod.html";
+  return;
+}
 
 try {
   const response = await fetch("hadith.json");
